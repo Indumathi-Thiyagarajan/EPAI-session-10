fc12ae61fc8c726f9634514d96f5afc02b685344
Indu Thiyagrajan <80294512+Indumathi-Thiyagarajan@users.noreply.github.com>
2024-08-27 17:28:30 -0700

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -1,69 +1,56 @@
 import math
 
 class Polygon:
-    def __init__(self, vertices, circumradius):
-        self.vertices = vertices
-        self.circumradius = circumradius
-
+    def __init__(self, n, R):
+        if n < 3:
+            raise ValueError('Polygon must have at least 3 vertices.')
+        self._n = n
+        self._R = R
+        
+    def __repr__(self):
+        return f'Polygon(n={self._n}, R={self._R})'
+    
     @property
-    def edges(self):
-        return self.vertices
-
+    def count_vertices(self):
+        return self._n
+    
+    @property
+    def count_edges(self):
+        return self._n
+    
+    @property
+    def circumradius(self):
+        return self._R
+    
     @property
     def interior_angle(self):
-        return (self.vertices - 2) * 180 / self.vertices
+        return (self._n - 2) * 180 / self._n
 
     @property
-    def edge_length(self):
-        return 2 * self.circumradius * math.sin(math.pi / self.vertices)
-
+    def side_length(self):
+        return 2 * self._R * math.sin(math.pi / self._n)
+    
     @property
     def apothem(self):
-        return self.circumradius * math.cos(math.pi / self.vertices)
-
+        return self._R * math.cos(math.pi / self._n)
+    
     @property
     def area(self):
-        return 0.5 * self.vertices * self.edge_length * self.apothem
-
+        return self._n / 2 * self.side_length * self.apothem
+    
     @property
     def perimeter(self):
-        return self.vertices * self.edge_length
-
-    @property
-    def efficiency(self):
-        return self.area / self.perimeter
-
-    def __repr__(self):
-        return (f'Polygon(vertices={self.vertices}, circumradius={self.circumradius}, '
-                f'edge_length={self.edge_length:.2f}, apothem={self.apothem:.2f}, '
-                f'area={self.area:.2f}, perimeter={self.perimeter:.2f})')
-
+        return self._n * self.side_length
+    
     def __eq__(self, other):
-        if isinstance(other, Polygon):
-            return self.vertices == other.vertices and self.circumradius == other.circumradius
-        return False
-
+        if isinstance(other, self.__class__):
+            return (self.count_edges == other.count_edges 
+                    and self.circumradius == other.circumradius)
+        else:
+            return NotImplemented
+        
     def __gt__(self, other):
-        if isinstance(other, Polygon):
-            return self.vertices > other.vertices
-        return False
-
-class PolygonSequence:
-    def __init__(self, max_vertices, circumradius):
-        self.max_vertices = max_vertices
-        self.circumradius = circumradius
-        self._polygons = [Polygon(i, circumradius) for i in range(3, max_vertices + 1)]
-
-    def __len__(self):
-        return len(self._polygons)
-
-    def __getitem__(self, s):
-        return self._polygons[s]
-
-    @property
-    def max_efficiency_polygon(self):
-        return max(self._polygons, key=lambda p: p.efficiency)
-
-    def __repr__(self):
-        return (f'PolygonSequence(max_vertices={self.max_vertices}, circumradius={self.circumradius}, '
-                f'polygons={len(self._polygons)})')
+        if isinstance(other, self.__class__):
+            return self.count_vertices > other.count_vertices
+        else:
+            return NotImplemented
